d84916b29e2b868ef18884dc19622f26d0a34fec
THINKPAD-MUSABI\Musabii <musabii@live.com>
2024-02-16 07:45:22 +0300

    ready

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import os
 import zipfile
+import http.client
+from datetime import datetime
 
-zip_file_name = "./backup/my_archive.zip"
+def send_msg_tg(msg):
+    conn = http.client.HTTPSConnection("api.telegram.org")
+    api = ""
+    chat_id = "275702269"
+    conn.request("GET", f"/bot{api}/sendMessage?chat_id={chat_id}&text={msg}")
+    response = conn.getresponse()
+    result = response.read()
+    print(result.decode("utf-8"))
+
+current_datetime = datetime.now()
+formatted_datetime = current_datetime.strftime("%d_%m_%Y")
+
+zip_file_name = f"{formatted_datetime}.zip"
+zip_dir = "./backup"
+zipfile_path = os.path.join(zip_dir, zip_file_name)
+
+source_file_name = "file.1CD"
 source_dir = "./db"
+sourcefile_path = os.path.join(source_dir, source_file_name)
 
-with zipfile.ZipFile(zip_file_name, "w", zipfile.ZIP_DEFLATED) as zip_object:
-    for root, dirs, files in os.walk(source_dir):
-        for file in files:
-            file_path = os.path.join(root, file)
-            zip_object.write(file_path, os.path.relpath(file_path, source_dir))
+
+
+try:
+    with zipfile.ZipFile(zipfile_path, "a", zipfile.ZIP_DEFLATED) as zip_object:
+        zip_object.write(sourcefile_path,source_file_name)
+    send_msg_tg("ok")
+except:
+    send_msg_tg("wrong")
+
+
+
